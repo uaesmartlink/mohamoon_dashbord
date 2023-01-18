ccd8db34ea549c103abc2c93a96d0f201d210d63
Abd Albary Tarakji <abd.tarakji96@gmail.com>
2023-01-18 17:45:50 +0400

    init project

--- src/configs/FirebaseConfig.js
+++ src/configs/FirebaseConfig.js
@@ -1,9 +1,9 @@
 const FirebaseConfig = {
   apiKey: "AIzaSyBeR0K7mLo3WoRzUxFa_q7vEpIHvZSwAFs",
-  authDomain: "lawyer-fe045.firebaseapp.com",
-  databaseURL: "https://lawyer-fe045-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "lawyer-fe045",
-  storageBucket: "lawyer-fe045.appspot.com",
+  authDomain: "mohamoon-f35de.firebaseapp.com",
+  databaseURL: "https://mohamoon-f35de-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mohamoon-f35de",
+  storageBucket: "mohamoon-f35de.appspot.com",
   messagingSenderId: "171602941313",
   appId: "1:171602941313:web:9baed3844746a615ffc480",
   measurementId: "G-87Z2BWDREC",
